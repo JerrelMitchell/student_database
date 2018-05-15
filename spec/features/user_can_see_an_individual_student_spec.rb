dc628878fc1c9c9f91a4e8a54a193bf553f53c45
JerrelMitchell <jerrelmitchell@ymail.com>
2018-05-15 15:54:12 -0600

    add address relationship and feature test

--- spec/features/user_can_see_an_individual_student_spec.rb
+++ spec/features/user_can_see_an_individual_student_spec.rb
@@ -8,4 +8,28 @@ describe 'As a user visiting a specific student page' do
 
     expect(page).to have_content(student.name)
   end
+
+  scenario 'displays addresses for one student' do
+    student = Student.create!(name: 'Tonya Sonyason')
+    address1 = student.addresses.create!(description: 'this way', street: 'street1', city: 'Denver', state: 'CO', zip_code: 123456)
+    address2 = student.addresses.create!(description: 'that way', street: 'street2', city: 'Boulder', state: 'TX', zip_code: 654321)
+
+    visit student_path(student)
+
+    save_and_open_page
+
+    expect(page).to have_content(student.name)
+
+    expect(page).to have_content(address1.description)
+    expect(page).to have_content(address1.street)
+    expect(page).to have_content(address1.city)
+    expect(page).to have_content(address1.state)
+    expect(page).to have_content(address1.zip_code)
+
+    expect(page).to have_content(address2.description)
+    expect(page).to have_content(address2.street)
+    expect(page).to have_content(address2.city)
+    expect(page).to have_content(address2.state)
+    expect(page).to have_content(address2.zip_code)
+  end
 end
--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -5,7 +5,7 @@ describe Student, type: :model do
     it { should validate_presence_of(:name) }
   end
 
-  describe 'relationship' do
+  describe 'relationships' do
     it { should have_many(:addresses) }
   end
 end
